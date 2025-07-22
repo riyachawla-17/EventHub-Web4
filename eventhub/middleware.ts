79d5224fc38a6bc7a77fb8281ae455be94dc78a9
RIYA CHAWLA <riyachawla203@gmail.com>
2025-07-21 23:11:50 -0400

    deploying

--- eventhub/middleware.ts
+++ eventhub/middleware.ts
@@ -1,34 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
-import jwt from "jsonwebtoken";
 
 export function middleware(req: NextRequest) {
   const token = req.cookies.get("token")?.value;
   const url = req.nextUrl.clone();
-  const pathname = req.nextUrl.pathname;
 
   if (!token) {
     url.pathname = "/login";
     return NextResponse.redirect(url);
   }
 
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET!) as any;
-
-    if (pathname.startsWith("/adminDashboard") && decoded.role !== "admin") {
-      url.pathname = "/login";
-      return NextResponse.redirect(url);
-    }
-
-    if (pathname.startsWith("/userDashboard") && decoded.role !== "user") {
-      url.pathname = "/login";
-      return NextResponse.redirect(url);
-    }
-
-    return NextResponse.next();
-  } catch {
-    url.pathname = "/login";
-    return NextResponse.redirect(url);
-  }
+  return NextResponse.next();
 }
 
 export const config = {
